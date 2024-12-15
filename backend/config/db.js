caaaa679f8d49916f5b1c3b1c83b380e2160f9b0
narendra <nareparmar@gmail.com>
2024-12-16 01:03:45 +0530

    first commiit

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import colors from 'colors';
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb+srv://parmar1jan1983:v71YusEWIfwjo8Y6@cluster0.jxoln.mongodb.net/expocon-db?retryWrites=true&w=majority&appName=Cluster0");
+        const conn = await mongoose.connect(process.env.MONGO_URL);
         console.log("Connected to the database".bgMagenta);
 
     } catch (error) {
